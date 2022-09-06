c9c0e34ea65b7926cbeb36f68c3c33b08cef463c
Allister Beharry <allister.beharry@gmail.com>
2022-09-06 13:07:51 +0000

    Print logo.

--- main.go
+++ main.go
@@ -6,12 +6,16 @@ import (
 	"github.com/alecthomas/kong"
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/mbndr/figlet4go"
+
+	"github.com/allisterb/citizen5/util"
 )
 
 type PingCmd struct {
 	Name  string `arg:"" name:"name" help:"Object id or key name to generate 128-bit object id for."`
 	Parse bool   `help:"Parse name as 128-bit object id." short:"P"`
 }
+
+var log = logging.Logger("CLI")
 
 // Command-line arguments
 var CLI struct {
@@ -27,10 +31,10 @@ func main() {
 		figlet4go.ColorYellow,
 		figlet4go.ColorCyan,
 	}
-	renderStr, _ := ascii.RenderOpts("Go-Ds-Motr", options)
+	renderStr, _ := ascii.RenderOpts("citizen5", options)
 	fmt.Print(renderStr)
 	ctx := kong.Parse(&CLI)
-	if util.contains(ctx.Args, "--debug") {
+	if util.Contains(ctx.Args, "--debug") {
 		logging.SetAllLoggers(logging.LevelInfo)
 		log.Info("Debug mode enabled.")
 	}
--- util/util.go
+++ util/util.go
@@ -1,6 +1,6 @@
 package util
 
-func contains(s []string, e string) bool {
+func Contains(s []string, e string) bool {
 	for _, a := range s {
 		if a == e {
 			return true
