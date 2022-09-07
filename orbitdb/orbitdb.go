c7668c6427f34ea5e9f4ced2ce7318a2ddd8ae27
Allister Beharry <allister.beharry@gmail.com>
2022-09-07 09:31:55 +0000

    Add most of ipfs imports.

--- orbitdb/orbitdb.go
+++ orbitdb/orbitdb.go
@@ -5,17 +5,17 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 
-	"github.com/libp2p/go-libp2p-core/crypto"
-	"github.com/libp2p/go-libp2p-core/peer"
-
 	ds "github.com/ipfs/go-datastore"
 	dsync "github.com/ipfs/go-datastore/sync"
+	iface "github.com/ipfs/interface-go-ipfs-core"
 	cfg "github.com/ipfs/kubo/config"
+	ipfsCore "github.com/ipfs/kubo/core"
 	"github.com/ipfs/kubo/repo"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/peer"
 )
 
-func initRepo(ctx context.Context) repo.Repo {
-
+func initIPFSRepo(ctx context.Context) repo.Repo {
 	c := cfg.Config{}
 	priv, pub, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, rand.Reader)
 	if err != nil {
@@ -41,5 +41,32 @@ func initRepo(ctx context.Context) repo.Repo {
 	return &repo.Mock{
 		D: dsync.MutexWrap(ds.NewMapDatastore()),
 		C: c,
+	}
+}
+
+func createIPFSAPIs(ctx context.Context, count int) ([]iface.CoreAPI, func()) {
+	coreAPIs := make([]iface.CoreAPI, count)
+	cleans := make([]func(), count)
+
+	for i := 0; i < count; i++ {
+		core, err := ipfsCore.NewNode(ctx, &ipfsCore.BuildCfg{
+			Online: true,
+			Repo:   initIPFSRepo(ctx),
+			ExtraOpts: map[string]bool{
+				"pubsub": true,
+			},
+		})
+		//require.NoError(t, err)
+
+		//coreAPIs[i] = testingCoreAPI(t, core)
+		cleans[i] = func() {
+			core.Close()
+		}
+	}
+
+	return coreAPIs, func() {
+		for i := 0; i < count; i++ {
+			cleans[i]()
+		}
 	}
 }
